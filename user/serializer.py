55f8a9df2ae919544a74b735f7e940b8be3b2cde
samrityy <shubhadon7@gmail.com>
2024-04-08 00:56:50 +0545

    fix: add create

--- user/serializer.py
+++ user/serializer.py
@@ -2,12 +2,20 @@ from rest_framework import serializers
 from .models import CustomUser,Cart
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password')
-
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+    def create(self,validated_data):
+        password=validated_data.pop('password',None)
+        instance=self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+        
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
