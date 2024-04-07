ef26459b38ed9f6022f351fee6abf0324fca2184
samrityy <shubhadon7@gmail.com>
2024-04-08 00:55:52 +0545

    fix:changed username_field from username to email

--- user/models.py
+++ user/models.py
@@ -2,7 +2,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    name=models.CharField(max_length=255)
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length=255)
+    username=None
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
 
 class Cart(models.Model):
     product = models.ManyToManyField("product.Product")
