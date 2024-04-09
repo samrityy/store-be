dbfb9912d3db1e45ed40a5673ae61ea050f963ec
Samrityy <smritymulmi23@gmail.com>
2024-04-09 10:26:57 +0545

    feat: add CustomUserManager instead of UserManger

--- user/models.py
+++ user/models.py
@@ -1,16 +1,19 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import UserManager
+from .manager import CustomUserManager
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     name=models.CharField(max_length=255)
     email=models.EmailField(unique=True)
     password=models.CharField(max_length=255)
     username=models.CharField(max_length=255, blank=True)
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=[]
-    objects=UserManager()
+    objects=CustomUserManager()
 
+    def __str__(self):
+        return self.email
+    
 class Cart(models.Model):
     product = models.ManyToManyField("product.Product")
     user = models.OneToOneField("CustomUser", on_delete=models.CASCADE)
