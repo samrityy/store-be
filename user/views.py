5cd9b0f4fcce219d9c6914d101fed063cd70aa77
Samrityy <smritymulmi23@gmail.com>
2024-04-11 12:47:57 +0545

    feat:add token in login

--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import CustomUser,Cart
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from .serializer import UserSerializer, CartSerializer
 class RegisterView(APIView):
@@ -14,15 +15,19 @@ class RegisterView(APIView):
         return Response(serializer.data)
 
 class LoginView(APIView):
-        def post(self,request):
-            email=request.data['email']
-            password=request.data['password']
-            user=CustomUser.objects.filter(email=email).first()
-            if user is None:
-                raise AuthenticationFailed('User not found')
-            if not user.check_password(password):
-                raise AuthenticationFailed('Incorrect password')
-            return Response({'message':'Success'})
+    authentication_classes = [TokenAuthentication]
+    def post(self,request):
+        email=request.data['email']
+        password=request.data['password']
+        user=CustomUser.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token':token.key})
+        
+
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
