06dc4ba91610c6255deddec88e79274e303d3df8
Samrityy <smritymulmi23@gmail.com>
2024-04-11 12:48:34 +0545

    feat: add token in login

--- user/models.py
+++ user/models.py
@@ -1,15 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from .manager import CustomUserManager
+from rest_framework.authtoken.models import Token
 
 class CustomUser(AbstractUser):
-    name=models.CharField(max_length=255)
-    email=models.EmailField(unique=True)
-    password=models.CharField(max_length=255)
-    username=models.CharField(max_length=255, blank=True)
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=[]
-    objects=CustomUserManager()
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, blank=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def get_or_create_token(self):
+        token, created = Token.objects.get_or_create(user=self)
+        return token.key
 
     def __str__(self):
         return self.email
