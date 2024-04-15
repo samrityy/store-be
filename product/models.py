73291ddf38fc2bdacc4f226f0de6575c2187c80d
Samrityy <smritymulmi23@gmail.com>
2024-04-15 09:53:50 +0545

    fix: change image into URL field

--- product/models.py
+++ product/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=225)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
+    description = models.CharField(max_length=20)
     category = models.CharField(max_length=225)
-    image = models.ImageField(upload_to="images/")
+    image = models.URLField()
     rating = models.JSONField()
 
     def __str__(self):
