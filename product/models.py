eac41f843c427903b329ee8b0ab66101cee81f86
Samrityy <smritymulmi23@gmail.com>
2024-04-19 23:20:54 +0545

    fix : removed double import

--- product/models.py
+++ product/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-
-from django.db import models
-
-
 class Product(models.Model):
     title = models.CharField(max_length=225)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -10,7 +6,6 @@ class Product(models.Model):
     category = models.CharField(max_length=225)
     image = models.ImageField(upload_to="images/")
     rating = models.JSONField()
-
     def __str__(self):
         return self.title
 
