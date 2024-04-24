4171edff8dd68a091e3177131dd45fd0711a7449
Samrityy <smritymulmi23@gmail.com>
2024-04-24 17:58:49 +0545

    feat: add cart in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CustomUser
+from .models import CustomUser,Cart
 admin.site.register(CustomUser)
+admin.site.register(Cart)
