298d913f6d25722202e6361cbfb929a52d9614c7
Samrityy <smritymulmi23@gmail.com>
2024-04-24 17:57:14 +0545

    feat: added feild product

--- user/serializer.py
+++ user/serializer.py
@@ -34,37 +34,10 @@ class UserSerializer(serializers.ModelSerializer):
 class CartSerializer(serializers.ModelSerializer):
     product_id = serializers.PrimaryKeyRelatedField(
         read_only=True
-    )  # Field to accept product id from frontend
+    )
     quantity = serializers.IntegerField(default=1)
 
     class Meta:
         model = CartLine
-        fields = ["product_id", "quantity"]
+        fields = ["product_id", "quantity","product"]
 
-    # def create(self, validated_data):
-    #     user = self.context["request"].user
-    #     cart = Cart.objects.get_or_create(user=user)
-    # product_ids = validated_data.pop("product_id")
-    # # import ipdb; ipdb.set_trace()
-    # user = self.context["request"].user
-    # cart = Cart.objects.get_or_create(user=user)
-
-    # if user.cart.exists():
-    #     cart = user.cart.first()
-    #     cart.product.add(*product_ids)
-    #     return cart
-    # else:
-    #     cart = Cart.objects.create(user=user)
-    #     cart.product.add(*product_ids)
-    #     return cart
-    # product = Product.objects.get(pk=product_id)
-    # cart_item = Cart.objects.create(product=product, **validated_data)
-    # return cart
-
-    # def destroy(self,request,pk):
-    #     try:
-    #         cart=Cart.objects.get(pk=pk)
-    #         cart.delete()
-    #         return Response({'message':'Deleted'})
-    #     except Cart.DoesNotExist:
-    #         return Response({'message':'Cart not found'})
