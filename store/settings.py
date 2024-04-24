394883a8302e075d57370a69ae52048f02c5411e
Samrityy <smritymulmi23@gmail.com>
2024-04-24 17:56:25 +0545

    fix: use of postgres instead of dbsqlite3

--- store/settings.py
+++ store/settings.py
@@ -92,11 +92,14 @@ CORS_ORIGIN_WHITELIST = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cart',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
